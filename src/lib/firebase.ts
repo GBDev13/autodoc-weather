0090a8db0726a3cf971b8236f2f48aae2fb88a9b
Gabriel Borges <gbdesigns13@gmail.com>
2022-08-19 12:45:17 -0300

    #change - Firebase in .env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,21 +3,13 @@ import { getDatabase } from 'firebase/database'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  // apiKey: import.meta.env.VITE_API_KEY,
-  // authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  // databaseURL: import.meta.env.VITE_DATABASE_URL,
-  // projectId: import.meta.env.VITE_PROJECT_ID,
-  // storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  // messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  // appId: import.meta.env.VITE_APP_ID,
-
-  apiKey: 'AIzaSyDsHjQevuN8cDuSE1yODxB5dQeYDE231yA',
-  authDomain: 'autodoc-91079.firebaseapp.com',
-  databaseURL: 'https://autodoc-91079-default-rtdb.firebaseio.com',
-  projectId: 'autodoc-91079',
-  storageBucket: 'autodoc-91079.appspot.com',
-  messagingSenderId: '1020658188525',
-  appId: '1:1020658188525:web:df1f210a26f494ce120ce4',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
